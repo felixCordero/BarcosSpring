ea6565d679b767c7d72bc85ced53dffe55f305af
Felix <daldinen@gmail.com>
2024-06-27 03:12:02 +0200

    Lombok modificado, agregado barco y amarre, conexion funcionando

--- src/main/java/com/barcos/Main.java
+++ src/main/java/com/barcos/Main.java
@@ -1,13 +1,37 @@
 package com.barcos;
 
+import com.barcos.model.Amarre;
+import com.barcos.model.Barco;
+import com.barcos.utils.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Barco barco = new Barco();
+        barco.setNombre("Barco Ejemplo");
+        barco.setTipo("Velero");
+        barco.setEslora(10);
+        barco.setManga(3);
+        barco.setCapacidad(6);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        Amarre amarre = new Amarre();
+        amarre.setUbicacion("A-1");
+        amarre.setPrecio(1500.0);
+        amarre.setProfundidad(5);
+        amarre.setLongitud(12);
+        amarre.setElectricidad(true);
+
+        barco.setAmarre(amarre);
+        amarre.setBarco(barco);
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.persist(barco);
+            session.persist(amarre);
+            transaction.commit();
         }
     }
 }
